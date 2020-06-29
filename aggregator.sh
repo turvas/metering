b5d510863ab7b698aa1c59449fe2608f0b5f9c49
kalev kuusik <kalev@bytelife.ee>
2020-06-29 17:00:31 +0300

    added summary and input error handling

--- aggregator.sh
+++ aggregator.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 # 1000 puslses = 1kwh
-FILE=$1
+if [ -z "$1" ]; then
+        echo Usage $0 measurements_file_path
+        exit 1
+else
+        FILE=$1
+fi
+MSUM=0
 for day in {1..31}; do
         if [ $day -lt 10 ]; then
                 day=0$day
@@ -8,6 +14,7 @@ for day in {1..31}; do
         LINES=`grep -a ^[0-9][0-9]\/$day $FILE`
         echo Date: $day
         if [ -n "$LINES" ]; then
+                DSUM=0
                 for hr in {0..23}; do
                         if [ $hr -lt 10 ]; then
                                 hr=0$hr
@@ -18,6 +25,10 @@ for day in {1..31}; do
                                 ((SUM += m))
                         done
                         echo $hr $SUM
+                        ((DSUM += SUM)) # day summary update
                 done
+                echo "Total ($day): $DSUM"
         fi
+        ((MSUM += DSUM)) # add daily to monthone
 done
+echo "Total (month): $MSUM"
