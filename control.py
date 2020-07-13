2038393040c2cd692dc98074cec400453e9f5138
kalev <ygi@pildistaja.com>
2020-07-13 18:09:19 +0300

    added basic scedule creation and relay control

--- control.py
+++ control.py
@@ -1,10 +1,11 @@
 import os
+import datetime
 
 # by hour, index is hr
 transahinnad = [0.0158, 0.0158, 0.0158, 0.0158, 0.0158, 0.0158, 0.0158, 0.0158, 0.0274, 0.0274, 0.0274, 0.0274, 0.0274, 0.0274, 0.0274, 0.0274, 0.0274, 0.0274, 0.0274, 0.0274, 0.0274, 0.0274, 0.0274, 0.0274, 0.0274]
 # tunni kohta arvutatud
 taastuvenergiatasu = 0.0113
-# kuutasu jagatud tunni peale
+# kuutasu jagatud tunni peale (25A siin)
 ampritasu = 0.0112
 
 
@@ -14,7 +15,6 @@ def isFloat(value):
         return True
     except ValueError:
         return False
-
 
 # out list od 24 elements, kwh
 def readFile(fn):
@@ -33,8 +33,8 @@ def readFile(fn):
     f.close()
     return borsihind
 
-
 # in list of 24 elements of borsihind
+# out list of 24 elements of real price, incl time sensitive trans, and other fix fees
 def calcPrice(borsihind):
     # type: (borsihind) -> list
     global transahinnad, taastuvenergiatasu, ampritasu
@@ -43,13 +43,58 @@ def calcPrice(borsihind):
     for raw in borsihind:
         hind = raw + transahinnad[hr] + taastuvenergiatasu + ampritasu
         hinnad.append(hind)
-        hr = hr + 1
+        hr += 1
     return hinnad
 
+# default relay is (fail-)closed, connected.
+# if needed to save power, (most of time), it will be opened/disconnected
+def controlRelay(relayID, scheduleOpen, hr=-1):
+    if hr == -1:    # not simulation/testing
+        now = datetime.datetime.now()
+        hrs = now.strftime("%H")  # string, hour 24h, localtime
+        hr = int(hrs)
+    else:           # simulation/testing
+        hrs = str(hr)
+    if scheduleOpen[hr] == True:        # activate relay => disconnect load by relay
+        print(hrs + " opening relay")
+    else:
+        print(hrs + " relay stay connected")
 
-pwd = os.getcwd()
+# Price extractor for pricing dict
+def getPrice(e):
+  return e['price']
+
+# in power kW (max. average/hr, not peak),
+# daily_consumption kWh, prices list in Eur
+#out list of 24, with True (open realy) or False (leave connected)
+def createSchedule(power, daily_consumption, prices):
+    priceDict = []
+    hr = 0
+    for hind in prices:   # prepare list of dictionary items for sorting
+        di = {'hour': hr, 'price': hind}
+        priceDict.append(di)
+        hr += 1
+    priceDict.sort(key=getPrice)    # cheapest hours first
+    i = consumed = 0
+    relayOpen = []
+    for hr in range(24):            # fill with disconnected state all time (save power)
+        relayOpen.append(True)
+    while consumed < daily_consumption: # iterate list sorted by cheap hours (relay load connected)
+        di = priceDict[i]
+        hr = di['hour']
+        relayOpen[hr] = False
+        consumed += power
+        i += 1
+    return relayOpen
+
+#pwd = os.getcwd()
 #print("PWD=" + pwd)
+# downloadFile
 borsihinnad = readFile("nps-export.csv")
 hinnad = calcPrice(borsihinnad)
-print(hinnad)
+#print(hinnad)
+schedule1 = createSchedule(2, 10, hinnad)
+for hr in range(24): # simulation
+    controlRelay(1, schedule1, hr)
 print("valmis")
+
