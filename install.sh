33116fb827b4d144b305bf20bf2f8ad3969b7230
kalev <ygi@pildistaja.com>
2020-07-19 22:30:57 +0300

    updated install script to include web interface

--- install.sh
+++ install.sh
@@ -9,3 +9,10 @@ sudo systemctl enable metering
 sudo cp control.service /etc/systemd/system/control.service
 sudo systemctl start control
 sudo systemctl enable control
+
+# sharing logs directory with web/apache
+sudo apt install apache2 -y
+cd /var/www/html
+sudo ln -s /var/metering
+sudo mv index.html index-deb.html
+sudo ln -s /opt/metering/index.html
