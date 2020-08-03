818b246da07a325600a003504e73096484253634
kalev <ygi@pildistaja.com>
2020-08-03 13:51:03 +0300

    added requirements.txt

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-pip install schedule requests
+pip install -r requirements.txt
+pip3 install -r requirements.txt
 
 sudo cp metering.service /etc/systemd/system/metering.service
 sudo systemctl start metering
