326c12244799786f8388ecd20fea612d98292d6b
kalev <ygi@pildistaja.com>
2020-08-03 10:38:49 +0300

    changed load consumption parameters

--- control.py
+++ control.py
@@ -30,8 +30,8 @@ hinnad = []  # list 24, kwh cost by hr
 schedules = []  # list of schedules (which are lists)
 # power kW, consumption in Kwh, hrStart2 in 24h system
 loads = [
-    {'name': 'boiler1', 'gpioPin': 17, 'power': 2, 'daily_consumption': 7, 'hrStart2': 15, 'consumption2': 1},
-    {'name': 'boiler2', 'gpioPin': 27, 'power': 2, 'daily_consumption': 7, 'hrStart2': 15, 'consumption2': 1}
+    {'name': 'boiler1', 'gpioPin': 17, 'power': 2, 'daily_consumption': 9, 'hrStart2': 15, 'consumption2': 3},
+    {'name': 'boiler2', 'gpioPin': 27, 'power': 2, 'daily_consumption': 9, 'hrStart2': 15, 'consumption2': 3}
 ]
 relays = []
 # in shell
